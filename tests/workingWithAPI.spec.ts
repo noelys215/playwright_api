46d9e3b5c2cd6faf5ecce4886d996fc5c5bb8051
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-12-31 09:55:05 -0500

    Mock API

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,9 +1,22 @@
 import { test, expect } from '@playwright/test';
+import tags from '../test-data/tags.json';
 
 test.beforeEach(async ({ page }) => {
+	await page.route('*/**/api/tags', async (route) => {
+		await route.fulfill({
+			body: JSON.stringify(tags),
+		});
+	});
 	await page.goto('https://conduit.bondaracademy.com/');
 });
 
 test('has title', async ({ page }) => {
 	await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+	await page.waitForTimeout(500);
 });
+
+/*
+App: https://conduit.bondaracademy.com/
+		https://conduit-api.bondaracademy.com
+API: https://conduit-api.bondaracademy.com/api/
+ */
