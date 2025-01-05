8337a5e8c0945383f988e765fde7576fbc6520b9
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-01-05 09:30:53 -0500

    modify api response

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -7,11 +7,25 @@ test.beforeEach(async ({ page }) => {
 			body: JSON.stringify(tags),
 		});
 	});
+
+	await page.route('*/**/api/articles*', async (route) => {
+		const response = await route.fetch();
+		const responseBody = await response.json();
+		responseBody.articles[0].title = 'Test title';
+		responseBody.articles[0].description = 'Test description';
+
+		await route.fulfill({ body: JSON.stringify(responseBody) });
+	});
+
 	await page.goto('https://conduit.bondaracademy.com/');
 });
 
 test('has title', async ({ page }) => {
 	await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+	await page.waitForTimeout(500);
+	await expect(page.locator('app-article-list h1').first()).toContainText('Test title');
+	await page.waitForTimeout(500);
+	await expect(page.locator('app-article-list p').first()).toContainText('Test description');
 	await page.waitForTimeout(500);
 });
 
