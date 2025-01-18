d6a0bfff1e0b598df846d99a1b276d07f6df90d4
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-01-17 19:52:53 -0500

    perform api resquest 1

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,32 +1,49 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 import tags from '../test-data/tags.json';
 
-test.beforeEach(async ({ page }) => {
-	await page.route('*/**/api/tags', async (route) => {
-		await route.fulfill({
-			body: JSON.stringify(tags),
-		});
-	});
+// test.beforeEach(async ({ page }) => {
+// 	await page.route('*/**/api/tags', async (route) => {
+// 		await route.fulfill({
+// 			body: JSON.stringify(tags),
+// 		});
+// 	});
 
+// 	await page.goto('https://conduit.bondaracademy.com/');
+// });
+
+test('has title', async ({ page }) => {
 	await page.route('*/**/api/articles*', async (route) => {
 		const response = await route.fetch();
 		const responseBody = await response.json();
-		responseBody.articles[0].title = 'Test title';
-		responseBody.articles[0].description = 'Test description';
+		responseBody.articles[0].title = 'Test a mock test title';
+		responseBody.articles[0].description = 'Test mock test description';
 
 		await route.fulfill({ body: JSON.stringify(responseBody) });
 	});
 
-	await page.goto('https://conduit.bondaracademy.com/');
-});
+	await page.getByText('Global Feed').click();
 
-test('has title', async ({ page }) => {
 	await expect(page.locator('.navbar-brand')).toHaveText('conduit');
 	await page.waitForTimeout(500);
-	await expect(page.locator('app-article-list h1').first()).toContainText('Test title');
+	await expect(page.locator('app-article-list h1').first()).toContainText(
+		'Test a mock test title'
+	);
 	await page.waitForTimeout(500);
-	await expect(page.locator('app-article-list p').first()).toContainText('Test description');
+	await expect(page.locator('app-article-list p').first()).toContainText(
+		'Test mock test description'
+	);
 	await page.waitForTimeout(500);
+});
+
+test('delete article', async ({ page, request }) => {
+	const response = await request.post('https://conduit.bondaracademy.com/login', {
+		data: {
+			user: { email: 'lavalamps@gmail.com', password: 'test123' },
+		},
+	});
+
+	const responseBody = await response.json();
+	console.log(responseBody);
 });
 
 /*
