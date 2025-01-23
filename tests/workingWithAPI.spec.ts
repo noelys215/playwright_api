13ed4050820933bf57c8768df9df5e43c803f208
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-01-22 20:16:10 -0500

    sharing auth state

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -9,10 +9,6 @@ test.beforeEach(async ({ page }) => {
 	});
 
 	await page.goto('https://conduit.bondaracademy.com/');
-	await page.getByText('Sign in').click();
-	await page.getByRole('textbox', { name: 'Email' }).fill('lavalamps@gmail.com');
-	await page.getByRole('textbox', { name: 'Password' }).fill('test123');
-	await page.getByRole('button').click();
 });
 
 test('has title', async ({ page }) => {
